d988835081cd53d78bcf60459c6c93f2d0476a33
Nafisa06 <nafisa.ila@gmail.com>
2021-12-30 02:05:31 +1100

    set questions for the squeeze and set the correct answer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// setting selectors 
+// declaring variables and setting selectors 
 
 var question = document.querySelector('#question');
 var choices = Array.from(document.querySelectorAll('.choice-text'));
@@ -11,4 +11,39 @@ let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
-/
+//setting questions as an array
+
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choice1: '<js>',
+        choice2: '<javascript>',
+        choice3: '<script>',
+        choice4: '<scripting>',
+        answer: 3,
+    },
+    {
+        question:"How can you add a comment in a JavaScript?",
+        choice1: "//*this is a commeny*//",
+        choice2: "'this is a comment",
+        choice3: "<!--this is a comment-->",
+        choice4: "None of the above",
+        answer: 4,
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        choice1: "var names=1= ('tom'), 2=('harry')",
+        choice2: "var names= (1:'tom', 2:'harry)",
+        choice3: "var names = ['tom', 'harry']",
+        choice4: "var names= 'tom', 'harry'",
+        answer: 3,
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choice1: "var hairColour",
+        choice2: "variable hairColour",
+        choice3: "v hairColour",
+        choice4: "var= hairColour",
+        answer: 1,
+    }
+]
