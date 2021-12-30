19bc8af02de71c7a7394b1241da2e0153b6ed42b
Nafisa06 <nafisa.ila@gmail.com>
2021-12-31 00:24:37 +1100

    added timer code

--- script.js
+++ script.js
@@ -132,4 +132,26 @@ decrementScore = num => {
     scoreText.innerText = score
 }
 
+//Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + "seconds left";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      
+    }
+  }, 1000);
+}
+
 startGame()
