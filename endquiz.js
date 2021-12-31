21ac1799a6b529bdd9925ca60ae70a44c6e2c09c
Nafisa06 <nafisa.ila@gmail.com>
2021-12-31 18:39:36 +1100

    code added to save the score and push it to the highscore page

--- endquiz.js
+++ endquiz.js
@@ -15,3 +15,32 @@ var MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
+// coding to save username and enable the submit button once the a username is added//
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+// saving the high score//
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    var score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+
+    
+}
